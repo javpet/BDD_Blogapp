6a382c1a48446dd547904862cba3794faf003e8f
javpet <javorkai.peter@gmail.com>
2018-06-30 12:17:21 +0200

    Added bootstrap

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.feature "Creating Articles" do
+RSpec.feature 'Creating Articles' do
   # Defining scenario
-  scenario "A user creates a new article" do
+  scenario 'A user creates a new article' do
     # Coming from Capybara -- conditions
-    visit "/"
-    click_link("New Article")
-    fill_in("Title", with: "Creating a blog")
-    fill_in("Body", with: "Lorem Ipsum")
-    click_button("Create Article")
+    visit '/'
+    click_link('New Article')
+    fill_in('Title', with: 'Creating a blog')
+    fill_in('Body', with: 'Lorem Ipsum')
+    click_button('Create Article')
 
     # Coming from Capybara -- expectations
-    expect(page).to have_content("Article has been created")
+    expect(page).to have_content('Article has been created')
     expect(page.current_path).to eq(articles_path)
   end
 end
